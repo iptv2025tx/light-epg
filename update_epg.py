18b9e550f01b5fbeefea39546482d905f39d5792
BuddyChewChew <144280901+BuddyChewChew@users.noreply.github.com>
2026-03-18 13:50:06 -0400

    Fresh EPG Update: Thu Mar 19 20:44:56 UTC 2026

--- update_epg.py
+++ update_epg.py
@@ -26,6 +26,7 @@ URLS = [
     'https://epgshare01.online/epgshare01/epg_ripper_DUMMY_CHANNELS.xml.gz',
     'https://raw.githubusercontent.com/BuddyChewChew/localnow-playlist-generator/refs/heads/main/epg.xml',
     'https://github.com/matthuisman/i.mjh.nz/raw/master/Plex/all.xml.gz',
+    'https://raw.githubusercontent.com/BuddyChewChew/dummy-epg-project/refs/heads/main/epg.xml',
     'https://raw.githubusercontent.com/matthuisman/i.mjh.nz/refs/heads/master/PlutoTV/all.xml'
 ]
 
