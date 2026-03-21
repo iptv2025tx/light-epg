29b6eed86051c29c46df86c71f06a927ac0b8461
2026 <iptv2025tx@gmail.com>
2026-03-20 21:52:17 -0500

    Fresh EPG Update: Sat Mar 21 03:53:24 UTC 2026

--- update_epg.py
+++ update_epg.py
@@ -22,11 +22,8 @@ URLS = [
     'https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS1.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_CA2.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_UK1.xml.gz',
-    'https://github.com/matthuisman/i.mjh.nz/raw/refs/heads/master/nzau/epg.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_DUMMY_CHANNELS.xml.gz',
-    'https://raw.githubusercontent.com/BuddyChewChew/localnow-playlist-generator/refs/heads/main/epg.xml',
     'https://github.com/matthuisman/i.mjh.nz/raw/master/Plex/all.xml.gz',
-    'https://raw.githubusercontent.com/BuddyChewChew/dummy-epg-project/refs/heads/main/epg.xml',
     'https://github.com/matthuisman/i.mjh.nz/raw/master/Roku/all.xml',
     'https://raw.githubusercontent.com/matthuisman/i.mjh.nz/refs/heads/master/PlutoTV/all.xml'
 ]
